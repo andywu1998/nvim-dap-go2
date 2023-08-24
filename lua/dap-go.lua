4e0c66e65debefe703832b2e64596802054f52c8
andywu1998 <1078539713@qq.com>
2023-08-24 14:08:00 +0800

    fmt

--- lua/dap-go.lua
+++ lua/dap-go.lua
@@ -1,17 +1,17 @@
 local M = {
-  last_testname = "",
-  last_testpath = "",
-  test_buildflags = "",
+    last_testname = "",
+    last_testpath = "",
+    test_buildflags = "",
 }
 
 local default_config = {
-  delve = {
-    path = "dlv",
-    initialize_timeout_sec = 20,
-    port = "${port}",
-    args = {},
-    build_flags = "",
-  },
+    delve = {
+        path = "dlv",
+        initialize_timeout_sec = 20,
+        port = "${port}",
+        args = {},
+        build_flags = "",
+    },
 }
 
 local tests_query = [[
@@ -36,274 +36,274 @@ local subtests_query = [[
 ]]
 
 local function load_module(module_name)
-  local ok, module = pcall(require, module_name)
-  assert(ok, string.format("dap-go dependency error: %s not installed", module_name))
-  return module
+    local ok, module = pcall(require, module_name)
+    assert(ok, string.format("dap-go dependency error: %s not installed", module_name))
+    return module
 end
 
 local function get_arguments()
-  local co = coroutine.running()
-  if co then
-    return coroutine.create(function()
-      local args = {}
-      vim.ui.input({ prompt = "Args: " }, function(input)
-        args = vim.split(input or "", " ")
-      end)
-      coroutine.resume(co, args)
-    end)
-  else
-    local args = {}
-    vim.ui.input({ prompt = "Args: " }, function(input)
-      args = vim.split(input or "", " ")
-    end)
-    return args
-  end
+    local co = coroutine.running()
+    if co then
+        return coroutine.create(function()
+            local args = {}
+            vim.ui.input({ prompt = "Args: " }, function(input)
+                args = vim.split(input or "", " ")
+            end)
+            coroutine.resume(co, args)
+        end)
+    else
+        local args = {}
+        vim.ui.input({ prompt = "Args: " }, function(input)
+            args = vim.split(input or "", " ")
+        end)
+        return args
+    end
 end
 
 local function filtered_pick_process()
-  local opts = {}
-  vim.ui.input(
-    { prompt = "Search by process name (lua pattern), or hit enter to select from the process list: " },
-    function(input)
-      opts["filter"] = input or ""
-    end
-  )
-  return require("dap.utils").pick_process(opts)
+    local opts = {}
+    vim.ui.input(
+        { prompt = "Search by process name (lua pattern), or hit enter to select from the process list: " },
+        function(input)
+            opts["filter"] = input or ""
+        end
+    )
+    return require("dap.utils").pick_process(opts)
 end
 
 local function setup_delve_adapter(dap, config)
-  local args = { "dap", "-l", "127.0.0.1:" .. config.delve.port }
-  vim.list_extend(args, config.delve.args)
+    local args = { "dap", "-l", "127.0.0.1:" .. config.delve.port }
+    vim.list_extend(args, config.delve.args)
 
-  dap.adapters.go = {
-    type = "server",
-    port = config.delve.port,
-    executable = {
-      command = config.delve.path,
-      args = args,
-    },
-    options = {
-      initialize_timeout_sec = config.delve.initialize_timeout_sec,
-    },
-  }
+    dap.adapters.go = {
+        type = "server",
+        port = config.delve.port,
+        executable = {
+            command = config.delve.path,
+            args = args,
+        },
+        options = {
+            initialize_timeout_sec = config.delve.initialize_timeout_sec,
+        },
+    }
 end
 
 local function setup_go_configuration(dap, configs)
-  dap.configurations.go = {
-    {
-      type = "go",
-      name = "Debug",
-      request = "launch",
-      program = "${file}",
-      buildFlags = configs.delve.build_flags,
-    },
-    {
-      type = "go",
-      name = "Debug (Arguments)",
-      request = "launch",
-      program = "${file}",
-      args = get_arguments,
-      buildFlags = configs.delve.build_flags,
-    },
-    {
-      type = "go",
-      name = "Debug Package",
-      request = "launch",
-      program = "${fileDirname}",
-      buildFlags = configs.delve.build_flags,
-    },
-    {
-      type = "go",
-      name = "Attach",
-      mode = "local",
-      request = "attach",
-      processId = filtered_pick_process,
-      buildFlags = configs.delve.build_flags,
-    },
-    {
-      type = "go",
-      name = "Debug test",
-      request = "launch",
-      mode = "test",
-      program = "${file}",
-      buildFlags = configs.delve.build_flags,
-    },
-    {
-      type = "go",
-      name = "Debug test (go.mod)",
-      request = "launch",
-      mode = "test",
-      program = "./${relativeFileDirname}",
-      buildFlags = configs.delve.build_flags,
-    },
-  }
+    dap.configurations.go = {
+        {
+            type = "go",
+            name = "Debug",
+            request = "launch",
+            program = "${file}",
+            buildFlags = configs.delve.build_flags,
+        },
+        {
+            type = "go",
+            name = "Debug (Arguments)",
+            request = "launch",
+            program = "${file}",
+            args = get_arguments,
+            buildFlags = configs.delve.build_flags,
+        },
+        {
+            type = "go",
+            name = "Debug Package",
+            request = "launch",
+            program = "${fileDirname}",
+            buildFlags = configs.delve.build_flags,
+        },
+        {
+            type = "go",
+            name = "Attach",
+            mode = "local",
+            request = "attach",
+            processId = filtered_pick_process,
+            buildFlags = configs.delve.build_flags,
+        },
+        {
+            type = "go",
+            name = "Debug test",
+            request = "launch",
+            mode = "test",
+            program = "${file}",
+            buildFlags = configs.delve.build_flags,
+        },
+        {
+            type = "go",
+            name = "Debug test (go.mod)",
+            request = "launch",
+            mode = "test",
+            program = "./${relativeFileDirname}",
+            buildFlags = configs.delve.build_flags,
+        },
+    }
 
-  if configs == nil or configs.dap_configurations == nil then
-    return
-  end
-
-  for _, config in ipairs(configs.dap_configurations) do
-    if config.type == "go" then
-      table.insert(dap.configurations.go, config)
+    if configs == nil or configs.dap_configurations == nil then
+        return
     end
-  end
+
+    for _, config in ipairs(configs.dap_configurations) do
+        if config.type == "go" then
+            table.insert(dap.configurations.go, config)
+        end
+    end
 end
 
 function M.setup(opts)
-  local config = vim.tbl_deep_extend("force", default_config, opts or {})
-  M.test_buildflags = config.delve.build_flags
-  local dap = load_module("dap")
-  setup_delve_adapter(dap, config)
-  setup_go_configuration(dap, config)
+    local config = vim.tbl_deep_extend("force", default_config, opts or {})
+    M.test_buildflags = config.delve.build_flags
+    local dap = load_module("dap")
+    setup_delve_adapter(dap, config)
+    setup_go_configuration(dap, config)
 end
 
 local function debug_test(testname, testpath, build_flags)
-  local dap = load_module("dap")
-  dap.run({
-    type = "go",
-    name = testname,
-    request = "launch",
-    mode = "test",
-    program = testpath,
-    args = { "-test.run", "^" .. testname .. "$" },
-    buildFlags = build_flags,
-  })
+    local dap = load_module("dap")
+    dap.run({
+        type = "go",
+        name = testname,
+        request = "launch",
+        mode = "test",
+        program = testpath,
+        args = { "-test.run", "^" .. testname .. "$" },
+        buildFlags = build_flags,
+    })
 end
 
 local function get_closest_above_cursor(test_tree)
-  local result
-  for _, curr in pairs(test_tree) do
-    if not result then
-      result = curr
-    else
-      local node_row1, _, _, _ = curr.node:range()
-      local result_row1, _, _, _ = result.node:range()
-      if node_row1 > result_row1 then
-        result = curr
-      end
+    local result
+    for _, curr in pairs(test_tree) do
+        if not result then
+            result = curr
+        else
+            local node_row1, _, _, _ = curr.node:range()
+            local result_row1, _, _, _ = result.node:range()
+            if node_row1 > result_row1 then
+                result = curr
+            end
+        end
     end
-  end
-  if result == nil then
-    return ""
-  elseif result.parent then
-    return string.format("%s/%s", result.parent, result.name)
-  else
-    return result.name
-  end
+    if result == nil then
+        return ""
+    elseif result.parent then
+        return string.format("%s/%s", result.parent, result.name)
+    else
+        return result.name
+    end
 end
 
 local function is_parent(dest, source)
-  if not (dest and source) then
-    return false
-  end
-  if dest == source then
-    return false
-  end
-
-  local current = source
-  while current ~= nil do
-    if current == dest then
-      return true
+    if not (dest and source) then
+        return false
+    end
+    if dest == source then
+        return false
     end
 
-    current = current:parent()
-  end
+    local current = source
+    while current ~= nil do
+        if current == dest then
+            return true
+        end
 
-  return false
+        current = current:parent()
+    end
+
+    return false
 end
 
 local function get_closest_test()
-  local stop_row = vim.api.nvim_win_get_cursor(0)[1]
-  local ft = vim.api.nvim_buf_get_option(0, "filetype")
-  assert(ft == "go", "dap-go error: can only debug go files, not " .. ft)
-  local parser = vim.treesitter.get_parser(0)
-  local root = (parser:parse()[1]):root()
+    local stop_row = vim.api.nvim_win_get_cursor(0)[1]
+    local ft = vim.api.nvim_buf_get_option(0, "filetype")
+    assert(ft == "go", "dap-go error: can only debug go files, not " .. ft)
+    local parser = vim.treesitter.get_parser(0)
+    local root = (parser:parse()[1]):root()
 
-  local test_tree = {}
+    local test_tree = {}
 
-  local test_query = vim.treesitter.query.parse(ft, tests_query)
-  assert(test_query, "dap-go error: could not parse test query")
-  for _, match, _ in test_query:iter_matches(root, 0, 0, stop_row) do
-    local test_match = {}
-    for id, node in pairs(match) do
-      local capture = test_query.captures[id]
-      if capture == "testname" then
-        local name = vim.treesitter.get_node_text(node, 0)
-        test_match.name = name
-      end
-      if capture == "parent" then
-        test_match.node = node
-      end
+    local test_query = vim.treesitter.query.parse(ft, tests_query)
+    assert(test_query, "dap-go error: could not parse test query")
+    for _, match, _ in test_query:iter_matches(root, 0, 0, stop_row) do
+        local test_match = {}
+        for id, node in pairs(match) do
+            local capture = test_query.captures[id]
+            if capture == "testname" then
+                local name = vim.treesitter.get_node_text(node, 0)
+                test_match.name = name
+            end
+            if capture == "parent" then
+                test_match.node = node
+            end
+        end
+        table.insert(test_tree, test_match)
     end
-    table.insert(test_tree, test_match)
-  end
 
-  local subtest_query = vim.treesitter.query.parse(ft, subtests_query)
-  assert(subtest_query, "dap-go error: could not parse test query")
-  for _, match, _ in subtest_query:iter_matches(root, 0, 0, stop_row) do
-    local test_match = {}
-    for id, node in pairs(match) do
-      local capture = subtest_query.captures[id]
-      if capture == "testname" then
-        local name = vim.treesitter.get_node_text(node, 0)
-        test_match.name = string.gsub(string.gsub(name, " ", "_"), '"', "")
-      end
-      if capture == "parent" then
-        test_match.node = node
-      end
+    local subtest_query = vim.treesitter.query.parse(ft, subtests_query)
+    assert(subtest_query, "dap-go error: could not parse test query")
+    for _, match, _ in subtest_query:iter_matches(root, 0, 0, stop_row) do
+        local test_match = {}
+        for id, node in pairs(match) do
+            local capture = subtest_query.captures[id]
+            if capture == "testname" then
+                local name = vim.treesitter.get_node_text(node, 0)
+                test_match.name = string.gsub(string.gsub(name, " ", "_"), '"', "")
+            end
+            if capture == "parent" then
+                test_match.node = node
+            end
+        end
+        table.insert(test_tree, test_match)
     end
-    table.insert(test_tree, test_match)
-  end
 
-  table.sort(test_tree, function(a, b)
-    return is_parent(a.node, b.node)
-  end)
+    table.sort(test_tree, function(a, b)
+        return is_parent(a.node, b.node)
+    end)
 
-  for _, parent in ipairs(test_tree) do
-    for _, child in ipairs(test_tree) do
-      if is_parent(parent.node, child.node) then
-        child.parent = parent.name
-      end
+    for _, parent in ipairs(test_tree) do
+        for _, child in ipairs(test_tree) do
+            if is_parent(parent.node, child.node) then
+                child.parent = parent.name
+            end
+        end
     end
-  end
 
-  return get_closest_above_cursor(test_tree)
+    return get_closest_above_cursor(test_tree)
 end
 
 function M.debug_test()
-  local testname = get_closest_test()
-  local relativeFileDirname = vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
-  local testpath = string.format("./%s", relativeFileDirname)
+    local testname = get_closest_test()
+    local relativeFileDirname = vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
+    local testpath = string.format("./%s", relativeFileDirname)
 
-  if testname == "" then
-    vim.notify("no test found")
-    return false
-  end
+    if testname == "" then
+        vim.notify("no test found")
+        return false
+    end
 
-  M.last_testname = testname
-  M.last_testpath = testpath
+    M.last_testname = testname
+    M.last_testpath = testpath
 
-  local msg = string.format("starting debug session '%s : %s'...", testpath, testname)
-  vim.notify(msg)
-  debug_test(testname, testpath, M.test_buildflags)
+    local msg = string.format("starting debug session '%s : %s'...", testpath, testname)
+    vim.notify(msg)
+    debug_test(testname, testpath, M.test_buildflags)
 
-  return true
+    return true
 end
 
 function M.debug_last_test()
-  local testname = M.last_testname
-  local testpath = M.last_testpath
+    local testname = M.last_testname
+    local testpath = M.last_testpath
 
-  if testname == "" then
-    vim.notify("no last run test found")
-    return false
-  end
+    if testname == "" then
+        vim.notify("no last run test found")
+        return false
+    end
 
-  local msg = string.format("starting debug session '%s : %s'...", testpath, testname)
-  vim.notify(msg)
-  debug_test(testname, testpath, M.test_buildflags)
+    local msg = string.format("starting debug session '%s : %s'...", testpath, testname)
+    vim.notify(msg)
+    debug_test(testname, testpath, M.test_buildflags)
 
-  return true
+    return true
 end
 
 return M
